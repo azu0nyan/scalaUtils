1a47ad48faad0656d900299420c2e5b390149ec2
azu0nyan <nikgavgav@gmail.com>
2020-06-17 01:11:49 +0300

    HeightGrid bakeToArray

--- src/main/scala/utils/datastructures/Grid.scala
+++ src/main/scala/utils/datastructures/Grid.scala
@@ -7,7 +7,7 @@ trait Grid[T] {
   def resolution: IntV2
 
   /**pos will be clamped to  [(0, 0), resolution - (1, 1)] */
-  def valueAt(pos: IntV2): T = valueAtUnsafe(clampResolutionIndices(pos))
+  @inline def valueAt(pos: IntV2): T = valueAtUnsafe(clampResolutionIndices(pos))
 
   /**do not call directly, use "valueAt" for safety check */
   def valueAtUnsafe(pos: IntV2): T
@@ -16,9 +16,11 @@ trait Grid[T] {
   final def clampResolutionIndices(divIndex: IntV2):IntV2 = IntV2(math.min(resolution.i - 1, math.max(divIndex.i, 0)), math.min(resolution.j - 1, math.max(divIndex.j, 0)))
 
   /**for storing grid in 1D array*/
-  def toFlatIndex(index: IntV2): Int = CircullarOps.circullarIndex((index.i * resolution.j) + index.j, valuesCount)
+  @inline def toFlatIndex(index: IntV2): Int = CircullarOps.circullarIndex((index.i * resolution.j) + index.j, valuesCount)
 
-  def valuesCount: Int = resolution.i * resolution.j
+  @inline def fromFlatIndex(index: Int): IntV2 = IntV2(index / resolution.j, index % resolution.j)
+
+  @inline def valuesCount: Int = resolution.i * resolution.j
 
   def indices: IterableOnce[IntV2] = for(i <- 0 until resolution.i; j<- 0 until resolution.j) yield IntV2(i, j)
 
--- src/main/scala/utils/heightmap/HeightGrid.scala
+++ src/main/scala/utils/heightmap/HeightGrid.scala
@@ -4,27 +4,41 @@ import utils.datastructures.{Grid, IntV2}
 import utils.math.space.{Plane, V3}
 import utils.math._
 
-trait HeightGrid extends Grid[Scalar]{
-//cells
+trait HeightGrid extends Grid[Scalar] {
+  //cells
   final def cells: IntV2 = IntV2(resolution.i - 1, resolution.j - 1)
 
-  final def clampCellIndices(cellIndex: IntV2):IntV2 = IntV2(math.min(cells.i - 1, math.max(cellIndex.i, 0)), math.min(cells.j - 1, math.max(cellIndex.j, 0)))
+  final def clampCellIndices(cellIndex: IntV2): IntV2 = IntV2(math.min(cells.i - 1, math.max(cellIndex.i, 0)), math.min(cells.j - 1, math.max(cellIndex.j, 0)))
 
-  def cellIndices: Seq[IntV2] = for(i <- 0 until cells.i; j<- 0 until cells.j) yield IntV2(i, j)
-//values
+  def cellIndices: Seq[IntV2] = for (i <- 0 until cells.i; j <- 0 until cells.j) yield IntV2(i, j)
+  //values
 
-  def toFlatHeightIndex(index: IntV2): Int = toFlatIndex(index)
+  @inline def toFlatHeightIndex(index: IntV2): Int = toFlatIndex(index)
 
-  def vertices(cordPlane:Plane):IterableOnce[V3] = indices.iterator.map(i => cordPlane.toWorldCords(i.toV2, valueAt(i)))
+  def vertices(cordPlane: Plane): IterableOnce[V3] = indices.iterator.map(i => cordPlane.toWorldCords(i.toV2, valueAt(i)))
 
   def maxHeight: Scalar = values.iterator.max
 
   def minHeight: Scalar = values.iterator.min
 
   //transforms
-  def scaled(scale:Int):HeightGrid = HeightGridScaled(this, scale)
+  def scaled(scale: Int): HeightGrid = HeightGridScaled(this, scale)
 
-//  def *(scale:Int):Tilemap = scaled(scale)
+  //  def *(scale:Int):Tilemap = scaled(scale)
 
-  def toHeightmap:Heightmap = HeightGridToHeightmap(this)
+  def toHeightmap: Heightmap = HeightGridToHeightmap(this)
+
+  def bakeToArray: HeightGrid = new HeightGrid {
+
+    /** Grid size, indices will be from (0, 0) to resolution - (1, 1) */
+   override val resolution: IntV2 = HeightGrid.this.resolution
+
+   val array:Array[Scalar] = Array.ofDim(valuesCount)
+    for(i <- 0 until resolution.area){
+      array(i) = HeightGrid.this.valueAt(fromFlatIndex(i))
+    }
+
+    /** do not call directly, use "valueAt" for safety check */
+    override def valueAtUnsafe(pos: IntV2): Scalar = array(toFlatIndex(pos))
+  }
 }
