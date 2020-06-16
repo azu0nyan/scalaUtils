58ae9d209361f05744de0c9d2b1be2d791134b20
azu0nyan <nikgavgav@gmail.com>
2020-06-17 02:11:40 +0300

    HeightGrid neighbours

--- src/main/scala/utils/datastructures/Grid.scala
+++ src/main/scala/utils/datastructures/Grid.scala
@@ -22,7 +22,15 @@ trait Grid[T] {
 
   @inline def valuesCount: Int = resolution.i * resolution.j
 
-  def indices: IterableOnce[IntV2] = for(i <- 0 until resolution.i; j<- 0 until resolution.j) yield IntV2(i, j)
+  def indices: Iterator[IntV2] = for(i <- 0 until resolution.i iterator; j<- 0 until resolution.j iterator) yield IntV2(i, j)
 
-  def values:IterableOnce[T] = indices.iterator.map(i => valueAt(i))
+  def values:Iterator[T] = indices.iterator.map(i => valueAt(i))
+
+  def neighboursXY(i:IntV2):Seq[IntV2] =
+    Seq(
+      Option.when(i.x > 0)(IntV2(i.x -1, i.y)),
+      Option.when(i.y > 0)( IntV2(i.x , i.y - 1)),
+      Option.when(i.x < resolution.x - 1)( IntV2(i.x +1, i.y)),
+      Option.when(i.y < resolution.y - 1)( IntV2(i.x , i.y + 1)),
+    ).flatten
 }
