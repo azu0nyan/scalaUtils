39e4e41589621d096cbeef564ac4a909834c0350
azu0nyan <nikgavgav@gmail.com>
2020-07-26 18:04:37 +0300

    memory conversions

--- src/main/scala/utils/mem/package.scala
+++ src/main/scala/utils/mem/package.scala
@@ -10,11 +10,27 @@ package object mem {
     override def bits: Long = count * multiplier
   }
 
-  case class  Bit(override val count: Long) extends MultipliedData(count, 1)
-  case class  Byte(override val count: Long) extends MultipliedData(count, 8)
+  implicit class toData(x:Long) {
+    def Bit:DataBit = DataBit(x)
+    def Byte:DataByte = DataByte(x)
+    def KiB:Kibibyte = Kibibyte(x)
+    def MiB:Mebibyte = Mebibyte(x)
+    def GiB:Gibibyte = Gibibyte(x)
+    def TiB:Tebibyte = Tebibyte(x)
+    def PiB:Pebibyte = Pebibyte(x)
+  }
+
+  type Kilobyte = Kibibyte
+  type Megabyte = Mebibyte
+  type Gigabyte = Kibibyte
+  type Terabyte = Tebibyte
+  type Petabyte = Pebibyte
+
+  case class  DataBit(override val count: Long) extends MultipliedData(count, 1)
+  case class  DataByte(override val count: Long) extends MultipliedData(count, 8)
   case class  Kibibyte(override val count: Long) extends MultipliedData(count, 8 * 1024)
   case class  Mebibyte(override val count: Long) extends MultipliedData(count, 8 * (1024 ^^ 2))
-  case class  Gibibyte(override val count: Long) extends MultipliedData(count, 1)
-  case class  Tebibyte(override val count: Long) extends MultipliedData(count, 1)
-  case class  Pebibyte(override val count: Long) extends MultipliedData(count, 1)
+  case class  Gibibyte(override val count: Long) extends MultipliedData(count, 8 * (1024 ^^ 3))
+  case class  Tebibyte(override val count: Long) extends MultipliedData(count, 8 * (1024 ^^ 4))
+  case class  Pebibyte(override val count: Long) extends MultipliedData(count, 8 * (1024 ^^ 5))
 }
