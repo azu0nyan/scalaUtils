7e5747d323e4d1d4bc35e2d8eaff11875d63d201
azu0nyan <nikgavgav@gmail.com>
2020-07-27 21:32:39 +0300

    initial

--- src/main/scala/utils/mem/package.scala
+++ src/main/scala/utils/mem/package.scala
@@ -1,23 +1,30 @@
 package utils
+
 import utils.math._
 
 package object mem {
   trait Data {
-    def bits:Long
+    def bits: Long
+    def bytes: Long = bits >> 3
+    def kilobytes: Long = bits >> (3 + 10L)
+    def megabytes: Long = bits >> (3 + 20L)
+    def gigabytes: Long = bits >> (3 + 30L)
+    def terabytes: Long = bits >> (3 + 40L)
+    def petabytes: Long = bits >> (3 + 50L)
   }
 
-  private class MultipliedData(val count:Long, val multiplier:Long) extends Data{
+  protected class MultipliedData(val count: Long, val multiplier: Long) extends Data {
     override def bits: Long = count * multiplier
   }
 
-  implicit class toData(x:Long) {
-    def Bit:DataBit = DataBit(x)
-    def Byte:DataByte = DataByte(x)
-    def KiB:Kibibyte = Kibibyte(x)
-    def MiB:Mebibyte = Mebibyte(x)
-    def GiB:Gibibyte = Gibibyte(x)
-    def TiB:Tebibyte = Tebibyte(x)
-    def PiB:Pebibyte = Pebibyte(x)
+  implicit class toData(x: Long) {
+    def Bit: DataBit = DataBit(x)
+    def Byte: DataByte = DataByte(x)
+    def KiB: Kibibyte = Kibibyte(x)
+    def MiB: Mebibyte = Mebibyte(x)
+    def GiB: Gibibyte = Gibibyte(x)
+    def TiB: Tebibyte = Tebibyte(x)
+    def PiB: Pebibyte = Pebibyte(x)
   }
 
   type Kilobyte = Kibibyte
@@ -26,11 +33,11 @@ package object mem {
   type Terabyte = Tebibyte
   type Petabyte = Pebibyte
 
-  case class  DataBit( count_ : Long) extends MultipliedData(count_, 1)
-  case class  DataByte( count_ : Long) extends MultipliedData(count_, 8)
-  case class  Kibibyte( count_ : Long) extends MultipliedData(count_, 8 * 1024)
-  case class  Mebibyte( count_ : Long) extends MultipliedData(count_, 8 * (1024 ^^ 2))
-  case class  Gibibyte( count_ : Long) extends MultipliedData(count_, 8 * (1024 ^^ 3))
-  case class  Tebibyte( count_ : Long) extends MultipliedData(count_, 8 * (1024 ^^ 4))
-  case class  Pebibyte( count_ : Long) extends MultipliedData(count_, 8 * (1024 ^^ 5))
+  case class DataBit(count_ : Long) extends MultipliedData(count_, 1)
+  case class DataByte(count_ : Long) extends MultipliedData(count_, 8)
+  case class Kibibyte(count_ : Long) extends MultipliedData(count_, 8 * 1024)
+  case class Mebibyte(count_ : Long) extends MultipliedData(count_, 8 * (1024 ^^ 2))
+  case class Gibibyte(count_ : Long) extends MultipliedData(count_, 8 * (1024 ^^ 3))
+  case class Tebibyte(count_ : Long) extends MultipliedData(count_, 8 * (1024 ^^ 4))
+  case class Pebibyte(count_ : Long) extends MultipliedData(count_, 8 * (1024 ^^ 5))
 }
