84fd1e762aff12f7bcabd44fb026affd5b337b2e
azu0nyan <nikgavgav@gmail.com>
2020-08-11 16:44:07 +0300

    Some java tasks pre java runner refactoring

--- src/main/scala/utils/RandomUtils.scala
+++ src/main/scala/utils/RandomUtils.scala
@@ -6,5 +6,7 @@ object RandomUtils extends RandomUtils
 
 trait RandomUtils {
 
+  def randomInt(fromInc:Int, toUninc:Int)(implicit seed: Int):Int = new Random(seed).nextInt(toUninc - fromInc) + fromInc
+
   def randomFromSeq[T](seq: Seq[T])(implicit seed: Int): T = seq(new Random(seed).nextInt(seq.size))
 }
