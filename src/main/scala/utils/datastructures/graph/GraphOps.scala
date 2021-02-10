7f04e5eb70d91057e71e4e9697e36adf39c87d2f
azu0nyan <nikgavgav@gmail.com>
2021-02-10 20:39:16 +0300

    graphs tasks

--- src/main/scala/utils/datastructures/graph/GraphOps.scala
+++ src/main/scala/utils/datastructures/graph/GraphOps.scala
@@ -24,4 +24,31 @@ object GraphOps {
 
     res
   }
+
+  def reachableFrom[ND, ED](g: Graph[ND, ED], vertex: ND): Seq[ND] = {
+    g.traversal(vertex).toSeq
+  }
+
+  def strongConnectivityComponentsOnDirectedGraph[ND, ED](g: Graph[ND, ED]): Seq[Seq[ND]] = {
+    var unvisited:Set[ND] = g.nodes.toSet
+    var res: Seq[Seq[ND]] = Seq()
+    while (unvisited.nonEmpty) {
+      val comp = g.traversal(unvisited.head).toSeq
+      res = comp +: res
+      unvisited = unvisited &~ comp.toSet
+    }
+    res
+  }
+
+  def fromAdjacencyMatrix[ND, ED](nodes:IndexedSeq[ND], edges:IndexedSeq[IndexedSeq[Option[ED]]]):Graph[ND, ED] = {
+    val g = new ArrayBufferGraph[ND, ED](false)
+    nodes.foreach(n => g.addNode(n))
+
+    for(i<- edges.indices; j <- edges(i).indices; e <- edges(i)(j)){
+      g.addEdge(nodes(i), nodes(j), e)
+    }
+
+    g
+  }
+
 }
