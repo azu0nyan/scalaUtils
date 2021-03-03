e783a11e72ff5f1448fd75782fb881dbcb38ac7a
azu0nyan <nikgavgav@gmail.com>
2021-03-04 00:48:30 +0300

    building gen progress

--- src/main/scala/utils/datastructures/dcel/PlanarDCEL.scala
+++ src/main/scala/utils/datastructures/dcel/PlanarDCEL.scala
@@ -7,17 +7,17 @@ import utils.math._
 
 
 object PlanarDCEL {
-  implicit class PlanarVertex[VD, HED, FD](f: RawVertex[VD, HED, FD])(implicit extractor:VD =>V2 ) {
+  implicit class PlanarVertex[VD, HED, FD](f: RawVertex[VD, HED, FD])(implicit extractor: VD => V2) {
     def pos: V2 = extractor(f.data)
     /** If vertex has no connected edges return face containing vertex */
     def insideFace(implicit dcel: PlanarDCEL[VD, HED, FD]): Option[RawFace[VD, HED, FD]] = Option.when(f.incidentEdge.isEmpty)(dcel.faceAt(pos))
   }
 
-  implicit class PlanarFace[VD, HED, FD](f: RawFace[VD, HED, FD]) (implicit extractor:VD =>V2 ){
+  implicit class PlanarFace[VD, HED, FD](f: RawFace[VD, HED, FD])(implicit extractor: VD => V2) {
     def polygon: PolygonRegion = PolygonRegion(f.outsideVertices.map(v => extractor(v.data)).toSeq)
   }
 
-  implicit class PlanarEdge[VD, HED, FD](e: RawHalfEdge[VD, HED, FD])(implicit extractor:VD =>V2 ) {
+  implicit class PlanarEdge[VD, HED, FD](e: RawHalfEdge[VD, HED, FD])(implicit extractor: VD => V2) {
     def asSegment: SegmentPlanar = SegmentPlanar(extractor(e.origin.data), extractor(e.dest.data))
 
   }
@@ -29,7 +29,6 @@ class PlanarDCEL[VD, HED, FD](
                              ) extends DCEL[VD, HED, FD](outerFaceData) {
 
 
-
   def pos(v: RawVertex[VD, HED, FD]): V2 = extractor(v.data)
 
   def seg(e: HalfEdge): SegmentPlanar = e.asSegment
@@ -37,7 +36,6 @@ class PlanarDCEL[VD, HED, FD](
   def outerContour(f: Face): PolygonRegion = PolygonRegion(f.outsideVertices.map(_.pos).toSeq)
 
   def polygon(f: Face): Polygon = Polygon(outerContour(f) +: f.holes.toSeq.map(h => PolygonRegion(h.traverseEdges.map(_.origin.pos).toSeq)))
-
 
 
   def faceAt(pos: V2): Face = {
@@ -65,66 +63,39 @@ class PlanarDCEL[VD, HED, FD](
               newEdProvider: (Vertex, Vertex) => (HED, HED),
               splitEdProvider: (HalfEdge, V2) => (HED, HED),
               newFdProvider: HalfEdge => FD,
-             ): Seq[Vertex] = cut(poly :+ poly.head, newVdProvider, newEdProvider, splitEdProvider, newFdProvider )
-  def cut(poly: Seq[V2],
-              newVdProvider: V2 => VD,
-              newEdProvider: (Vertex, Vertex) => (HED, HED),
-              splitEdProvider: (HalfEdge, V2) => (HED, HED),
-              newFdProvider: HalfEdge => FD,
-             ): Seq[Vertex] = {
+             ): Seq[Vertex] = cutChain(poly :+ poly.head, newVdProvider, newEdProvider, splitEdProvider, newFdProvider)
+
+
+  def getOrAddVertex(pos: V2, newVdProvider: V2 => VD, splitEdProvider: (HalfEdge, V2) => (HED, HED)): Vertex = {
+    val res = vertices.find(_.pos ~= pos)
+      .orElse(
+        halfEdges.find(_.asSegment.contains(pos)).map {
+          e =>
+            val (l, r) = splitEdProvider(e, pos)
+            val res = split(e, newVdProvider(pos), l, r)
+
+            res
+        }
+      ).getOrElse(makeVertex(newVdProvider(pos)))
+    res
+  }
+
+  def cutFromTo(from: V2, to: V2,
+                newVdProvider: V2 => VD,
+                newEdProvider: (Vertex, Vertex) => (HED, HED),
+                splitEdProvider: (HalfEdge, V2) => (HED, HED),
+                newFdProvider: HalfEdge => FD,
+               ): Seq[Vertex] = cutChain(Seq(from, to), newVdProvider, newEdProvider, splitEdProvider, newFdProvider)
+
+  def cutChain(poly: Seq[V2],
+               newVdProvider: V2 => VD,
+               newEdProvider: (Vertex, Vertex) => (HED, HED),
+               splitEdProvider: (HalfEdge, V2) => (HED, HED),
+               newFdProvider: HalfEdge => FD,
+              ): Seq[Vertex] = {
     var res: Seq[Vertex] = Seq()
 
-    def getOrAddVertex(pos: V2): Vertex = {
-      val res = vertices.find(_.pos ~= pos)
-        .orElse(
-          halfEdges.find(_.asSegment.contains(pos)).map {
-            e =>
-              val (l, r) = splitEdProvider(e, pos)
-              val res = split(e, newVdProvider(pos), l, r)
 
-              res
-          }
-        ).getOrElse(makeVertex(newVdProvider(pos)))
-      res
-    }
-
-    //finds
-    /*  def orderAsHoleIfNeeded(f: Face): Option[Face] = {
-        val poly = PolygonRegion(f.outsideVertices.map(_.pos).toSeq)
-
-        def descentPlaceHole(outer: Face) = {
-          val childs = outer.holes.filter(h => h.traverseEdges.map(_.origin.pos).forall(hv => poly.containsInside(hv)))
-          //if we have childs inside outer then we cant be child of its childs
-          if (childs.nonEmpty) {
-            childs.foreach { c =>
-              c._leftFace._holes -= c
-              f._holes += c
-            }
-            outer.h
-          } else {
-            //we can be child of some outer's childs
-            val containingHole = outer.holes.find {
-              h =>
-                val p = PolygonRegion(h.traverseEdges.map(_.origin.pos).toSeq)
-                f.vertices.forall(v => p.containsInside(v.pos))
-            }
-            if(containingHole.nonEmpty){
-              containingHole.get.allReachableFaces().find { f =>
-                val p = f.polygon
-                f.vertices.forall(v => p.classify(v.pos) == PolygonRegion.INSIDE)
-              } match {
-                case Some(containing) =>
-
-                case None =>
-              }
-            }
-          }
-
-        }
-
-        descentPlaceHole(outerFace)
-      }
-  */
     if (poly.size <= 1) return Seq()
 
     var toTraverse = poly.tail
@@ -132,7 +103,7 @@ class PlanarDCEL[VD, HED, FD](
 
 
     //    var currentPosition = startPosition
-    var previous = getOrAddVertex(startPosition)
+    var previous = getOrAddVertex(startPosition, newVdProvider, splitEdProvider)
 
     res = res :+ previous
 
@@ -142,25 +113,25 @@ class PlanarDCEL[VD, HED, FD](
 
       val toTest: Seq[HalfEdge] = if (previous.incidentEdge.isEmpty) {
         val face = faceAt(cur)
-//        println(if (face == outerFace) "outer" else face.data.toString)
-//        println(face.edges.map(_.asSegment.start).toSeq)
+        //        println(if (face == outerFace) "outer" else face.data.toString)
+        //        println(face.edges.map(_.asSegment.start).toSeq)
         face.edges.toSeq
       } else {
         previous.edgesWithOriginHere.map(_.leftFace).distinct.flatMap(_.edges.toSeq).toSeq
       }
 
       val path = SegmentPlanar(cur, end)
-//      println(toTest.map(_.asSegment))
+      //      println(toTest.map(_.asSegment))
       val intersections = toTest.flatMap(t => t.asSegment.intersection(path))
         .filter {
           case PointIntersection(p) => !(p ~= cur)
           case _ => true
         }
-//      println(path)
-//      println(intersections)
+      //      println(path)
+      //      println(intersections)
       if (intersections.isEmpty) {
         toTraverse = toTraverse.tail
-        getOrAddVertex(end)
+        getOrAddVertex(end, newVdProvider, splitEdProvider)
       } else {
         val closestIntersection = intersections.map {
           case PointIntersection(p) => p
@@ -173,7 +144,7 @@ class PlanarDCEL[VD, HED, FD](
         if (closestIntersection ~= end) {
           toTraverse = toTraverse.tail
         }
-        getOrAddVertex(closestIntersection)
+        getOrAddVertex(closestIntersection, newVdProvider, splitEdProvider)
       }
     }
 
@@ -184,17 +155,17 @@ class PlanarDCEL[VD, HED, FD](
 
       val start = previous.pos
       val end = current.pos
-//      println(s"from $start to $end")
+      //      println(s"from $start to $end")
 
       if (previous.incidentEdge.isEmpty && current.incidentEdge.isEmpty) {
-//        println("1")
+        //        println("1")
         //no edges or faces, we inside some face
         val face = faceAt(previous.pos)
         val (ld, rd) = newEdProvider(previous, current)
         val he = makeEdge(previous, current, face, face, ld, rd)
         face._holes += he
       } else if (previous.incidentEdge.nonEmpty && current.incidentEdge.isEmpty) {
-//        println("2")
+        //        println("2")
         //we are going inside face
         val face = faceAt(current.pos)
         //
@@ -206,7 +177,7 @@ class PlanarDCEL[VD, HED, FD](
           val v2 = estart - eend
           //min ccw
           val res = AngleOps.ccwAngleFromTo(v1, v2)
-//          println(estart, eend, v1, v2, res)
+          //          println(estart, eend, v1, v2, res)
           res
         }
 
@@ -217,13 +188,13 @@ class PlanarDCEL[VD, HED, FD](
         //        val (ld, rd) = newEdProvider(previous, current)
         //        makeEdge(previous, current, face, face, ld, rd)
       } else if (previous.incidentEdge.isEmpty && current.incidentEdge.nonEmpty) {
-//        println("3")
+        //        println("3")
         //we are going to edge from inside face
         val face = faceAt(previous.pos)
         val (ld, rd) = newEdProvider(previous, current)
         makeEdge(previous, current, face, face, ld, rd)
       } else {
-//        println("4")
+        //        println("4")
         //if vertices disconnected
         if (!previous.edgesWithOriginHere.exists(e => e.ending == current)) {
           //we are closing chain
@@ -252,10 +223,10 @@ class PlanarDCEL[VD, HED, FD](
               val v2 = eend - estart
               //min cw
               val res = -AngleOps.ccwAngleFromTo(v1, v2)
-//              println(estart, eend, v1, v2, res)
+              //              println(estart, eend, v1, v2, res)
               res
             }
-//            println(s"next ${nextEdge.data}")
+            //            println(s"next ${nextEdge.data}")
             val prevEdge = previous.edgesWithEndHere.minBy { e =>
               val v1 = end - start
               val estart = e.origin.pos
@@ -263,10 +234,10 @@ class PlanarDCEL[VD, HED, FD](
               val v2 = estart - eend
               //min ccw
               val res = AngleOps.ccwAngleFromTo(v1, v2)
-//              println(estart, eend, v1, v2, res)
+              //              println(estart, eend, v1, v2, res)
               res
             }
-//            println(s"prev ${prevEdge.data}")
+            //            println(s"prev ${prevEdge.data}")
 
 
             val newEdge = makeEdge(previous, current, prevEdge.leftFace, nextEdge.leftFace, ld, rd,
@@ -275,24 +246,24 @@ class PlanarDCEL[VD, HED, FD](
             //if left and right different polys
             if (!newEdge.traverseEdges.contains(newEdge.twin)) {
               //if we cut part of outer we should start from inner edge
-              val startFaceFrom = if(PolygonRegion(newEdge.traverseEdges.map(_.origin.pos).toSeq).isCcw) newEdge else newEdge.twin
+              val startFaceFrom = if (PolygonRegion(newEdge.traverseEdges.map(_.origin.pos).toSeq).isCcw) newEdge else newEdge.twin
 
-//              println(s"Making new face starting from ${startFaceFrom.data}")
+              //              println(s"Making new face starting from ${startFaceFrom.data}")
               val f = makeFace(newFdProvider(startFaceFrom), startFaceFrom, startFaceFrom.twin)
               val fPoly = f.polygon
               f.incidentEdge.flatMap(_.traverseAllReachableEdges().filter(_.isHoleHalfSide).nextOption()) match {
                 case Some(holeSide) =>
-//                  println(s"Face can contain holes ${holeSide.data} ${holeSide.leftFace.holes.map(_.data)}")
+                  //                  println(s"Face can contain holes ${holeSide.data} ${holeSide.leftFace.holes.map(_.data)}")
                   holeSide.leftFace.holes.filter(h =>
                     h.traverseEdges.map(_.origin.pos).forall(v => fPoly.classify(v) == PolygonRegion.INSIDE)
                   ).foreach {
                     hole =>
-//                      println(s"Found hole ${hole.data}")
+                      //                      println(s"Found hole ${hole.data}")
                       holeSide.leftFace._holes -= hole
                       f._holes += hole
-//                      println(holeSide.leftFace._holes.map(_.data))
-//                      println(f._holes.map(_.data))
-//                      println("Overriding old parent")
+                      //                      println(holeSide.leftFace._holes.map(_.data))
+                      //                      println(f._holes.map(_.data))
+                      //                      println("Overriding old parent")
                       hole.traverseEdges.foreach(he => he._leftFace = f)
 
                   }
@@ -303,7 +274,7 @@ class PlanarDCEL[VD, HED, FD](
             } else {
               //connected something inside leftFace, probably  two holes
               if (newEdge.leftFace == newEdge.twin.leftFace) {
-//                println("Connecting holes")
+                //                println("Connecting holes")
                 val leftFace = newEdge.leftFace
                 val holes = leftFace._holes.toSeq
                 if (holes.nonEmpty) {
