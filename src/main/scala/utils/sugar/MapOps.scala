03992da6aea015cd0f49f5bf8a158474283c4de0
azu0nyan <nikgavgav@gmail.com>
2021-04-21 12:33:27 +0300

    fixes

--- src/main/scala/utils/sugar/MapOps.scala
+++ src/main/scala/utils/sugar/MapOps.scala
@@ -2,14 +2,18 @@ package utils.sugar
 
 object MapOps {
 
-  def addSeqToMapToSet[A, B](map:Map[A, Set[B]], seq:Seq[(A, B)]):Map[A, Set[B]] = {
-    val seqMapped = seq.toMap
+  def addSeqToMapToSet[A, B](map: Map[A, Set[B]], seq: Seq[(A, B)]): Map[A, Set[B]] = {
+    println("add", map, seq)
+    val seqMapped: Map[A, Set[B]] = seq.groupBy(_._1)
+      .map { case (a, seq) => (a, seq.map(_._2).toSet) }
     val newKeys = seqMapped.keySet &~ map.keySet
     //todo maybe maybe optimize?
-    map.map{
-      case (a,set)  if seqMapped.contains(a) => (a, set ++  Set(seqMapped(a)))
+    val res = map.map {
+      case (a, set) if seqMapped.contains(a) => (a, set | seqMapped(a))
       case (a, set) => (a, set)
-    } ++ newKeys.map(k => (k, Set(seqMapped(k))))
+    } ++ newKeys.map(k => (k, seqMapped(k)))
+    println(res)
+    res
   }
 
 }
