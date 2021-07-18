b36ca8b9439fb60f8105c812bd8478a027b40321
azu0nyan <nikgavgav@gmail.com>
2021-07-18 12:06:20 +0300

    avg time calc refactoring

--- src/main/scala/utils/system/CalcExecTime.scala
+++ src/main/scala/utils/system/CalcExecTime.scala
@@ -1,15 +1,45 @@
 package utils.system
 
+import utils.math.Scalar
+
 object CalcExecTime {
-  def apply(code: => Unit):StopwatchSample = {
+
+
+  class CalcTimeStats(name:Option[String] = None) {
+
+    var runs: Long = 0
+    var timeTotalMs: Long = 0
+    var timeTotalNS: Long = 0
+
+    var lastSample: StopwatchSample = _
+
+    def avgMs: Scalar = timeTotalMs / runs.toDouble
+
+    def avgMsStr: String = f"$avgMs%.2f"
+
+    def statsStr: String = s"${name.getOrElse("")} last: ${lastSample.msStr} avg: $avgMsStr"
+
+    def apply[T](code: => T):T = {
+      val s = new Stopwatch
+      val res: T = code
+      lastSample = s.sampleNow()
+      runs += 1
+      timeTotalMs += lastSample.dt.dt
+      timeTotalNS += lastSample.dNs
+      res
+    }
+
+  }
+
+  def apply(code: => Unit): StopwatchSample = {
     val s = new Stopwatch
     code
     s.sampleNow()
   }
 
-  def withResult[T](code: => T):(T, StopwatchSample) = {
+  def withResult[T](code: => T): (T, StopwatchSample) = {
     val s = new Stopwatch
-    val res:T = code
+    val res: T = code
     (res, s.sampleNow())
   }
 
