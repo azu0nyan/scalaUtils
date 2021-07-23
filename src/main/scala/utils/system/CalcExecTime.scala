f847e7d39f40791d652266b839b3fa77ae74dc90
azu0nyan <nikgavgav@gmail.com>
2021-07-23 19:34:07 +0300

    some caching

--- src/main/scala/utils/system/CalcExecTime.scala
+++ src/main/scala/utils/system/CalcExecTime.scala
@@ -5,7 +5,7 @@ import utils.math.Scalar
 object CalcExecTime {
 
 
-  class CalcTimeStats(name:Option[String] = None) {
+  class CalcTimeStats(name:Option[String] = None, warmupResetRun:Option[Int] = None) {
 
     var runs: Long = 0
     var timeTotalMs: Long = 0
@@ -17,12 +17,17 @@ object CalcExecTime {
 
     def avgMsStr: String = f"$avgMs%.2f"
 
-    def statsStr: String = s"${name.getOrElse("")} last: ${lastSample.msStr} avg: $avgMsStr"
+    def statsStr: String = s"${name.getOrElse("")} runs: $runs last: ${lastSample.msStr} avg: $avgMsStr"
 
     def apply[T](code: => T):T = {
       val s = new Stopwatch
       val res: T = code
       lastSample = s.sampleNow()
+      if(warmupResetRun.contains(runs)){
+        runs = 0
+        timeTotalMs = 0
+        timeTotalNS = 0
+      }
       runs += 1
       timeTotalMs += lastSample.dt.dt
       timeTotalNS += lastSample.dNs
