321ea9ffd892b617c939263131e3bede1591c8d4
azu0nyan <nikgavgav@gmail.com>
2021-12-23 16:31:59 +0300

    archetypes

--- src/main/scala/utils/math/planar/SegmentPlanarOps.scala
+++ src/main/scala/utils/math/planar/SegmentPlanarOps.scala
@@ -2,8 +2,10 @@ package utils.math.planar
 
 import utils.math._
 
+import scala.collection.mutable
+
 object SegmentPlanarOps {
-  def haveCommonSegmentAt(fSeg:SegmentPlanar, tSeg:SegmentPlanar, length:Scalar, atFirstFraction:Scalar = 0.5 ):Boolean = {
+  def haveCommonSegmentAt(fSeg: SegmentPlanar, tSeg: SegmentPlanar, length: Scalar, atFirstFraction: Scalar = 0.5): Boolean = {
     fSeg.intersection(tSeg) match {
       case Some(SegmentIntersection(intersection)) =>
         val fractionOffset = length / fSeg.length * .5
@@ -12,5 +14,20 @@ object SegmentPlanarOps {
         intersection.contains(atFrom) && intersection.contains(atTo)
       case _ => false
     }
+  }
+
+  def groupByCollinearity(segs: Seq[SegmentPlanar]):Map[Scalar, Seq[SegmentPlanar]] = {
+    val res = mutable.Map[Scalar, mutable.Buffer[SegmentPlanar]]()
+    for (s <- segs) {
+      val a = {
+        val a = s.body.angleToOX
+        if (a >= 0) a else a + PI
+      }
+      res.keySet.find(_ ~= a) match {
+        case Some(angle) => res(angle) += s
+        case None => res += a -> mutable.Buffer(s)
+      }
+    }
+    res.view.mapValues(_.toSeq).toMap
   }
 }
