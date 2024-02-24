9cb897d6f1ea1890965269c6e8c878dc43a5eca2
azu0nyan <nikgavgav@gmail.com>
2024-02-24 19:19:14 +0300

    inverted layouts

--- src/main/scala/utils/mesh/MutableMeshSection.scala
+++ src/main/scala/utils/mesh/MutableMeshSection.scala
@@ -10,11 +10,11 @@ import utils.math.space.{Triangle, V3}
 import scala.collection.mutable.ArrayBuffer
 
 class MutableMeshSection(
-                  override val vertices: ArrayBuffer[V3],
-                  override val triangles: ArrayBuffer[(Int, Int, Int)],
-                  override val normals: ArrayBuffer[V3],
-                  override val uvs: ArrayBuffer[V2]
-                 ) extends MeshSection {
+                          override val vertices: ArrayBuffer[V3],
+                          override val triangles: ArrayBuffer[(Int, Int, Int)],
+                          override val normals: ArrayBuffer[V3],
+                          override val uvs: ArrayBuffer[V2]
+                        ) extends MeshSection {
 
   def this() = {
     this(ArrayBuffer(),
@@ -28,7 +28,7 @@ class MutableMeshSection(
     addOther(meshSection)
 
   }
-  def addOther(ot:MeshSection):MutableMeshSection = this ++= ot
+  def addOther(ot: MeshSection): MutableMeshSection = this ++= ot
 
 
   def ++=(ot: MeshSection): MutableMeshSection = {
@@ -41,20 +41,28 @@ class MutableMeshSection(
     this
   }
 
-  def invertSidesInPlace:MutableMeshSection = {
-    for(i <- triangles.indices){
+  def invertSidesInPlace: MutableMeshSection = {
+    for (i <- triangles.indices) {
       val tri = triangles(i)
       triangles(i) = (tri._1, tri._3, tri._2)
     }
-    for(i <- normals.indices){
+    for (i <- normals.indices) {
       normals(i) = -normals(i)
     }
     this
   }
 
+  def flipTrianglesToMatchNormal(otherNormal: V3): MutableMeshSection = {
+    for {
+      i <- triangles.indices;
+      (i1, i2, i3) = triangles(i)
+      triangle = Triangle(vertices(i1), vertices(i2), vertices(i3))
+      if triangle.flipToMatchNormal(otherNormal) != triangle
+    }
+      triangles(i) = (i1, i3, i2)
 
-
-
+    this
+  }
 
   }
 
