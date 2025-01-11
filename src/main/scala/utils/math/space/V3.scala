144f1e130caa2ab7456b7445a1f2fa5995aa7297
azu0nyan <nikgavgav@gmail.com>
2025-01-11 19:39:52 +0300

    angle fp error fix

--- src/main/scala/utils/math/space/V3.scala
+++ src/main/scala/utils/math/space/V3.scala
@@ -5,11 +5,11 @@ import utils.math.planar.V2
 
 object V3 extends V3Constants {
 
-  def apply(s:Scalar): V3 = from(s)
+  def apply(s: Scalar): V3 = from(s)
 
-  def apply[T](s:T)(implicit f:T => Scalar): V3 = from(f(s))
+  def apply[T](s: T)(implicit f: T => Scalar): V3 = from(f(s))
 
-  def from(s:Scalar):V3 = V3.fromScalars(s,s,s)
+  def from(s: Scalar): V3 = V3.fromScalars(s, s, s)
 
   def fromScalars(x: Scalar, y: Scalar, z: Scalar): V3 = V3(x, y, z)
 
@@ -21,7 +21,7 @@ object V3 extends V3Constants {
 
   def fromArray(a: Array[Scalar]): V3 = V3(a(0), a(1), a(2))
 
-  def fromArray[T](a: Array[T])(implicit f:T => Scalar): V3 = V3(f(a(0)), f(a(1)), f(a(2)))
+  def fromArray[T](a: Array[T])(implicit f: T => Scalar): V3 = V3(f(a(0)), f(a(1)), f(a(2)))
 
   def toNormal(v: V3): Normal = if (v.lengthSquared ~= 1f) v else v.normalize
 }
@@ -91,29 +91,29 @@ case class V3(x: Scalar, y: Scalar, z: Scalar) {
 
   @inline def unary_- : V3 = opposite
 
-  @inline def unit : V3 = normalize
+  @inline def unit: V3 = normalize
 
   @inline def opposite: V3 = V3(-x, -y, -z)
 
-  @inline def +(v: V3):V3 = V3(x + v.x, y + v.y, z + v.z)
+  @inline def +(v: V3): V3 = V3(x + v.x, y + v.y, z + v.z)
 
-  @inline def -(v: V3):V3 = V3(x - v.x, y - v.y, z - v.z)
+  @inline def -(v: V3): V3 = V3(x - v.x, y - v.y, z - v.z)
 
-  @inline def *(v: V3):V3 = V3(x * v.x, y * v.y, z * v.z)
+  @inline def *(v: V3): V3 = V3(x * v.x, y * v.y, z * v.z)
 
-  @inline def *(s: Scalar):V3 = V3(x * s, y * s, z * s)
+  @inline def *(s: Scalar): V3 = V3(x * s, y * s, z * s)
 
-  @inline def /(v: V3):V3 = V3(x / v.x, y / v.y, z / v.z)
+  @inline def /(v: V3): V3 = V3(x / v.x, y / v.y, z / v.z)
 
   @inline def -(): V3 = V3(-x, -y, -z)
 
   /** vector(cross) product */
-  @inline def ^(v: V3):V3 = V3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x)
+  @inline def ^(v: V3): V3 = V3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x)
 
   /** scalar product */
   @inline def **(v: V3): Scalar = x * v.x + y * v.y + z * v.z
 
-  /** almost equals*/
+  /** almost equals */
   @inline def ~=(v: V3): Boolean = (x ~= v.x) && (y ~= v.y) && (z ~= v.z)
 
   /** fast safe normalization, doing stuff only if needed */
@@ -126,8 +126,8 @@ case class V3(x: Scalar, y: Scalar, z: Scalar) {
     return this / length
   }
 
-  /**non oriented angle*/
-  @inline def angle(v: V3): Scalar = acos(this ** v / (this.length * v.length))
+  /** non oriented angle */
+  @inline def angle(v: V3): Scalar = acos(clamp(this ** v / (this.length * v.length), -1d, 1d))
 
   @inline def distance(v: V3): Scalar = (this - v).length
 
@@ -150,25 +150,25 @@ case class V3(x: Scalar, y: Scalar, z: Scalar) {
   @inline def heightCord: Scalar = y //todo heightComponent
 
   /** creating 2d vector dropping x */
-  @inline def dropX:V2 = V2(y, z)
+  @inline def dropX: V2 = V2(y, z)
 
   /** creating 2d vector dropping y */
-  @inline def dropY:V2 = V2(x, z)
+  @inline def dropY: V2 = V2(x, z)
 
   /** creating 2d vector dropping z */
-  @inline def dropZ:V2 = V2(x, y)
+  @inline def dropZ: V2 = V2(x, y)
 
-  @inline def replaceX(nx: Scalar) :V3= V3(nx, y, z)
+  @inline def replaceX(nx: Scalar): V3 = V3(nx, y, z)
 
-  @inline def replaceY(ny: Scalar) :V3= V3(x, ny, z)
+  @inline def replaceY(ny: Scalar): V3 = V3(x, ny, z)
 
-  @inline def replaceZ(nz: Scalar) :V3= V3(x, y, nz)
+  @inline def replaceZ(nz: Scalar): V3 = V3(x, y, nz)
 
   @inline def toSeq: Seq[Scalar] = Seq(x, y, z)
 
   @inline def toArray: Array[Scalar] = Array(x, y, z)
 
-  /** project vector on other vector v ** ||ot||*/
+  /** project vector on other vector v ** ||ot|| */
   @inline def projectionOn(ot: V3): Scalar = this ** V3.toNormal(ot.normalize)
 
   @inline def toBasis(basis: (V3, V3, V3)): V3 = toBasis(basis._1, basis._2, basis._3)
@@ -178,7 +178,7 @@ case class V3(x: Scalar, y: Scalar, z: Scalar) {
 
   @inline def fromBasis(basis: (V3, V3, V3)): V3 = fromBasis(basis._1, basis._2, basis._3)
 
-  /** (i * x) + (j * y) + (k * z)  */
+  /** (i * x) + (j * y) + (k * z) */
   @inline def fromBasis(i: V3, j: V3, k: V3): V3 = (i * x) + (j * y) + (k * z)
 
   //@ENGINE CONVERSION todo
