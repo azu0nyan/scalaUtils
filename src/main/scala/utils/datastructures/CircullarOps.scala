0e39dc8561a38ce02813c412c864984561c8fd0c
azu0nyan <nikgavgav@gmail.com>
2025-01-26 17:34:51 +0300

    fix angle accumulator

--- src/main/scala/utils/datastructures/CircullarOps.scala
+++ src/main/scala/utils/datastructures/CircullarOps.scala
@@ -4,12 +4,12 @@ import scala.collection.mutable.ArrayBuffer
 
 
 object CircullarOps {
-  implicit def toCyclic[A](a: Seq[A]):CircullarSeq[A] =new CircullarSeq(a)
+  implicit def toCyclic[A](a: Seq[A]): CircullarSeq[A] = new CircullarSeq(a)
 
-  implicit def toCyclic[A](a: Array[A]):CircullarArray[A] =new CircullarArray(a)
+  implicit def toCyclic[A](a: Array[A]): CircullarArray[A] = new CircullarArray(a)
 
   def toCyclicPairs[A](a: Seq[A]): Iterator[(A, A)] = {
-    (a :+ a.head).sliding(2).map{case Seq(a, b) => (a, b)}
+    (a :+ a.head).sliding(2).map { case Seq(a, b) => (a, b) }
   }
 
   @inline def circullarIndex(element: Int, length: Int): Int = utils.math.circullarIndex(element, length)
@@ -21,12 +21,22 @@ object CircullarOps {
 
   implicit class CircullarArray[A](a: Array[A]) {
     def getCircullar(id: Int): A = a(circullarIndex(id, a.length))
-    def setCircullar(el:A, id: Int): Unit = a(circullarIndex(id, a.length)) = el
+    def setCircullar(el: A, id: Int): Unit = a(circullarIndex(id, a.length)) = el
   }
-  
-  
+
+
   extension [A](a: Seq[A]) {
     def asCyclicPairs: Iterator[(A, A)] = toCyclicPairs(a)
+
+    def circularShifts: Iterator[Seq[A]] =
+      for (i <- a.indices.iterator)
+        yield a.drop(i) ++ a.take(i)
+
+
+    def existsWithShifts(p: Seq[A] => Boolean): Boolean =
+      circularShifts.exists(p)
+
+
   }
 
 }
