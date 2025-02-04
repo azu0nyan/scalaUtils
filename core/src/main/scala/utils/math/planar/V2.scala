c3372c9c6b43ed35185290d91e5fd24d1ce98a7f
azu0nyan <nikgavgav@gmail.com>
2025-02-04 23:22:50 +0300

    V2 swap and inline

--- core/src/main/scala/utils/math/planar/V2.scala
+++ core/src/main/scala/utils/math/planar/V2.scala
@@ -27,64 +27,64 @@ object V2 {
 case class V2(x: Scalar, y: Scalar) extends WithMap {
   def toProduct: (Scalar, Scalar) = (x, y)
 
-  @inline def reciporalSafe: V2 = V2(
+  inline def reciporalSafe: V2 = V2(
     1 / (if (x == 0) SMALL_NUMBER else x),
     1 / (if (y == 0) SMALL_NUMBER else y),
   )
 
 
-  @inline def apply(i: Int): Scalar = i match {
+  inline def apply(i: Int): Scalar = i match {
     case 0 => x
     case 1 => y
     case _ => throw new IndexOutOfBoundsException(s"$i out of bounds of vector")
   }
 
-  @inline def xInt: Int = x.toInt
+  inline def xInt: Int = x.toInt
 
-  @inline def yInt: Int = y.toInt
+  inline def yInt: Int = y.toInt
 
-  @inline def unary_- : V2 = opposite
+  inline def unary_- : V2 = opposite
 
-  @inline def opposite: V2 = V2(-x, -y)
+  inline def opposite: V2 = V2(-x, -y)
 
-  @inline def +(v: V2): V2 = V2(x + v.x, y + v.y)
+  inline def +(v: V2): V2 = V2(x + v.x, y + v.y)
 
-  @inline def -(v: V2): V2 = V2(x - v.x, y - v.y)
+  inline def -(v: V2): V2 = V2(x - v.x, y - v.y)
 
-  @inline def *(v: V2): V2 = V2(x * v.x, y * v.y)
+  inline def *(v: V2): V2 = V2(x * v.x, y * v.y)
 
-  @inline def *(s: Scalar): V2 = V2(x * s, y * s)
+  inline def *(s: Scalar): V2 = V2(x * s, y * s)
 
-  @inline def /(v: V2): V2 = V2(x / v.x, y / v.y)
+  inline def /(v: V2): V2 = V2(x / v.x, y / v.y)
 
-  @inline def -(): V2 = V2(-x, -y)
+  inline def -(): V2 = V2(-x, -y)
 
-  @inline def **(v: V2): Scalar = x * v.x + y * v.y
+  inline def **(v: V2): Scalar = x * v.x + y * v.y
 
-  @inline def det(v: V2): Scalar = x * v.y - y * v.x
+  inline def det(v: V2): Scalar = x * v.y - y * v.x
 
-  @inline def cross(v: V2): Scalar = x * v.y - y * v.x
+  inline def cross(v: V2): Scalar = x * v.y - y * v.x
 
-  @inline def ~=(v: V2): Boolean = (x ~= v.x) && (y ~= v.y)
+  inline def ~=(v: V2): Boolean = (x ~= v.x) && (y ~= v.y)
 
 
-  @inline def nonZero: Boolean = x != 0 || y != 0
+  inline def nonZero: Boolean = x != 0 || y != 0
 
-  @inline def nearZero: Boolean = this ~= V2.ZERO
+  inline def nearZero: Boolean = this ~= V2.ZERO
 
-  @inline def nonNearZero: Boolean = !nearZero
+  inline def nonNearZero: Boolean = !nearZero
 
-  @inline def normalize: V2 = if (length == 0) {
+  inline def normalize: V2 = if (length == 0) {
     V2(0, 0)
   } else {
     /(length)
   }
 
-  @inline def angleToOX: Scalar = math.atan2(y, x)
+  inline def angleToOX: Scalar = math.atan2(y, x)
 
-  @inline def angle(v: V2): Scalar = (math.atan2(v.y, v.x) - math.atan2(y, x))
+  inline def angle(v: V2): Scalar = (math.atan2(v.y, v.x) - math.atan2(y, x))
 
-  @inline def angleClampedToPi(v: V2): Scalar = {
+  inline def angleClampedToPi(v: V2): Scalar = {
     var a = angle(v)
     a = a % TWO_PI
     a = (a + TWO_PI) % TWO_PI;
@@ -102,57 +102,60 @@ case class V2(x: Scalar, y: Scalar) extends WithMap {
   }
 
 
-  @inline def distance(v: V2): Scalar = (this - v).length
+  inline def distance(v: V2): Scalar = (this - v).length
 
-  @inline def rotate90CCW: V2 = V2(-y, x)
+  inline def rotate90CCW: V2 = V2(-y, x)
 
-  @inline def rotate90CW: V2 = V2(y, -x)
+  inline def rotate90CW: V2 = V2(y, -x)
 
-  @inline def rotate(a: Scalar): V2 = V2(x * cos(a) - y * sin(a), x * sin(a) + y * cos(a))
+  inline def rotate(a: Scalar): V2 = V2(x * cos(a) - y * sin(a), x * sin(a) + y * cos(a))
 
-  @inline def rotateAroundPoint(rotation: Scalar, point: V2): V2 = (this - point).rotate(rotation) + point
+  inline def rotateAroundPoint(rotation: Scalar, point: V2): V2 = (this - point).rotate(rotation) + point
 
-  @inline def scaleAroundPoint(scale: Scalar, point: V2): V2 = (this - point) * scale + point
+  inline def scaleAroundPoint(scale: Scalar, point: V2): V2 = (this - point) * scale + point
 
-  @inline def lengthSquared: Scalar = this ** this
+  inline def lengthSquared: Scalar = this ** this
 
-  @inline def length: Scalar = math.hypot(x, y)
+  inline def length: Scalar = math.hypot(x, y)
 
   override def toString: String = s"""V2($x, $y)"""
 
   def toShortString: String = f"${toShortStr(x)}, ${toShortStr(y)}"
 
-  @inline def addZ(z: Scalar): V3 = V3(x, y, z)
+  inline def addZ(z: Scalar): V3 = V3(x, y, z)
 
-  @inline def addX(X: Scalar): V3 = V3(X, x, y)
+  inline def addX(X: Scalar): V3 = V3(X, x, y)
 
-  @inline def addY(Y: Scalar): V3 = V3(x, Y, y)
+  inline def addY(Y: Scalar): V3 = V3(x, Y, y)
 
-  @inline def toSeq: Seq[Scalar] = Seq(x, y)
+  /** swaps coordinate axes (x, y) -> (y, x) */
+  inline def swap: V2 = V2(y, x)
 
-  @inline def negateX: V2 = V2(-x, y)
+  inline def toSeq: Seq[Scalar] = Seq(x, y)
 
-  @inline def negateY: V2 = V2(x, -y)
+  inline def negateX: V2 = V2(-x, y)
 
-  @inline def planarToV3(upCord: Scalar): V3 = V3(x, upCord, -y)
+  inline def negateY: V2 = V2(x, -y)
 
-  @inline def clamp: (V2, V2) => V2 = utils.math.clamp(this, _, _)
+  inline def planarToV3(upCord: Scalar): V3 = V3(x, upCord, -y)
 
-  @inline def clampSnap: (V2, V2) => V2 = utils.math.clampSnap(this, _, _)
+  inline def clamp: (V2, V2) => V2 = utils.math.clamp(this, _, _)
 
-  @inline def toIntV2: IntV2 = IntV2(x.toInt, y.toInt)
+  inline def clampSnap: (V2, V2) => V2 = utils.math.clampSnap(this, _, _)
 
-  @inline def collinear(other: V2): Boolean = (normalize ~= other.normalize) || (normalize ~= other.normalize.opposite)
+  inline def toIntV2: IntV2 = IntV2(x.toInt, y.toInt)
+
+  inline def collinear(other: V2): Boolean = (normalize ~= other.normalize) || (normalize ~= other.normalize.opposite)
 
   //todo optimize and check
-//  @inline def sameDirection(other: V2): Boolean = y == 0 && other.y == 0 || (x / y ~= other.x / other.y)
-  @inline def sameDirection(other: V2): Boolean = normalize ~= other.normalize
+  //  inline  def sameDirection(other: V2): Boolean = y == 0 && other.y == 0 || (x / y ~= other.x / other.y)
+  inline def sameDirection(other: V2): Boolean = normalize ~= other.normalize
 
-  @inline def floor: V2 = V2(Math.floor(x), Math.floor(y))
+  inline def floor: V2 = V2(Math.floor(x), Math.floor(y))
 
-  @inline def ceil: V2 = V2(Math.ceil(x), Math.ceil(y))
+  inline def ceil: V2 = V2(Math.ceil(x), Math.ceil(y))
 
-  @inline def round: V2 = V2(Math.round(x), Math.round(y))
+  inline def round: V2 = V2(Math.round(x), Math.round(y))
 
 }
 
